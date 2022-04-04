51fcdc0a0aee3f3d9aa6fd16ce2a0e625172e604
bahare nikoosiyar <bahare.nikoosiyar@gmail.com>
2022-04-05 03:08:02 +0430

    home page

---
+++ src/js/app.js
@@ -0,0 +1,77 @@
+//slider
+
+const slider1 = document.querySelector('.playlist-wrapper1');
+const slides1 = slider1.children;
+const maxScrollLeft1 = slider1.scrollWidth - slider1.clientWidth;
+const slider2 = document.querySelector('.playlist-wrapper2');
+const slides2 = slider2.children;
+const maxScrollLeft2 = slider2.scrollWidth - slider2.clientWidth;
+
+
+function autoPlay(slider,maxScrollLeft) {
+    if (slider.scrollLeft > (maxScrollLeft - 1)) {
+        slider.scrollLeft -= maxScrollLeft;
+    } else {
+        slider.scrollLeft += 1;
+    }    
+}
+
+// //pause slider on hover
+const pauseSlider = function(slider,slides,maxScrollLeft) {
+    for (let slide of slides) {
+        slide.addEventListener('mouseover',()=> {
+            if (slider === slider1) {
+                clearInterval(play1);
+            } else if (slider === slider2) {
+                clearInterval(play2);
+            }
+        });
+        slide.addEventListener('mouseout',()=> {
+            if (slider === slider1) {
+                play1 = setInterval(function() {
+                    autoPlay(slider,maxScrollLeft);
+                },50);
+                return play1;
+            } else if (slider === slider2) { 
+                play2 = setInterval(function() {
+                    autoPlay(slider,maxScrollLeft);
+                },50);
+            }
+        })
+    }    
+}
+
+let play1 = setInterval(function() {
+    autoPlay(slider1,maxScrollLeft1);
+},50);
+
+let play2 = setInterval(function() {
+    autoPlay(slider2,maxScrollLeft2);
+},50);
+
+pauseSlider(slider1,slides1,maxScrollLeft1);
+
+pauseSlider(slider2,slides2,maxScrollLeft2);
+
+//carousel
+const carousel = document.querySelector('.carousel');
+const images = carousel.children;
+console.log(images);
+
+let carouselImageIndex = 0;
+
+const changeCarousel = function () {
+    images[carouselImageIndex].classList.toggle('active');
+
+    if (carouselImageIndex >= images.length - 1) {
+        carouselImageIndex = 0;
+    } else {
+        carouselImageIndex += 1;
+    }
+
+    images[carouselImageIndex].classList.toggle('active');
+}
+
+setInterval(() => {
+    changeCarousel();
+}, 3000);
---
+++ src/js/data.js
@@ -0,0 +1,129 @@
+let songs = [
+    {
+        name: 'Yadesh Bekheir',
+        path: 'assests\music\Sogand - Yadesh Bekheir.mp3',
+        artist: 'Sogand',
+        cover: 'assests\images\Sogand-Yadesh-Bekheir.jpg'
+    },
+    {
+        name: 'Bomb',
+        path: 'assests\music\Zakhmi & Sogand - Bomb.mp3',
+        artist: 'Zakhmi & sogand',
+        cover: 'assests\images\Sogand Ft Zakhmi - Bomb.jpg'
+    },
+    {
+        name: 'Tehran',
+        path: 'assests\music\Sogand - Tehran.mp3',
+        artist: 'Sogand',
+        cover: 'assests\images\sogand-tehran.jpg'
+    },
+    {
+        name: 'Dore Zamin',
+        path: 'assests\music\Sogand - Dore Zamin (Ft Paya).mp3',
+        artist: 'Sogand(Ft Paya)',
+        cover: 'assests\images\Sogand-Paya-–-Dore-Zamin.jpg'
+    },
+    {
+        name: 'Asabani',
+        path: 'assests\music\shayea @DAVAYDARD - Asabani.mp3',
+        artist: 'Shayea',
+        cover: 'assests\images\shayea-asabani.jpg'
+    },
+
+    {
+        name: 'Ki Boode',
+        path: 'assests\music\Shayea - Ki Boode.mp3',
+        artist: 'Shayea',
+        cover: 'assests\images\Shayea - Ki Boode.jpg'
+    },
+    {
+        name: 'Khalafkaraye Asli',
+        path: 'assests\music\Khalafkaraye-Asli-hichkas.mp3',
+        artist: 'Hichkas',
+        cover: 'assests\images\Hichkas - Khalafkaraye Asli.jpg'
+    },
+    {
+        name: 'Ye Moghehaei',
+        path: 'assests\music\11 Shayea Ft T-Dey - Ye Moghehaei.mp3',
+        artist: 'Shayea(Ft T-day)',
+        cover: 'assests\images\shayeat-dey-400x400-ye-moghehayi.jpg'
+    },
+    {
+        name: 'Man Bahat Ghahram',
+        path: 'assests\music\Amir Tataloo - Man Bahat Ghahram [320].mp3',
+        artist: 'Tataloo',
+        cover: 'assests\images\Amir-Tataloo-Man-Bahat-Ghahram.jpg'
+    },
+    {
+        name: 'Romantic',
+        path: 'assests\music\romantic-sogand.mp3',
+        artist: 'Sogand',
+        cover: 'assests\images\romantic-sogand.jpg'
+    },
+    {
+        name: 'Man',
+        path: 'assests\music\Ho3ein - Man [320].mp3',
+        artist: 'Ho3ein',
+        cover: 'assests\images\Ho3ein -Man.jpg'
+    },
+    {
+        name: 'Khakestri',
+        path: 'assests\music\Khakestari.mp3',
+        artist: 'Pishro',
+        cover: 'assests\images\Reza-Pishro-khakestari.jpg'
+    },
+    {
+        name: 'Baraye',
+        path: 'assests\music\Sadegh - Baraye (Ft Amir Khalvat) (128).mp3',
+        artist: 'Amir Khalvat (Ft Sadegh)',
+        cover: 'assests\images\Baraye-Amirkhalvat.jpg'
+    },
+    {
+        name: 'Poolo Ghodrat',
+        path: 'assests\music\Sadegh - Poolo Ghodrat 128.mp3',
+        artist: 'Sadegh',
+        cover: 'assests\images\Sadegh - Poolo Ghodrat.jpg'
+    },
+    {
+        name: 'Nakhoda Jelal',
+        path: 'assests\music\02 Nakhoda Jelal [Prod. Dehghan].mp3',
+        artist: 'ُSorena',
+        cover: 'assests\images\Sorena-Gavazn.jpg'
+    },
+    {
+        name: 'Boro Bala',
+        path: 'assests\music\Shayea - Boro Bala.mp3',
+        artist: 'ُShayea',
+        cover: 'assests\images\Shayea - Boro Bala.jpg'
+    },
+    {
+        name: 'Jadval O Roya',
+        path: 'assests\music\11 Jadval O Roya.mp3',
+        artist: 'ُHichkas',
+        cover: 'assests\images\hichkas-mahdyar-mojaz-rellmusic-810x810.jpg'
+    },
+    {
+        name: 'Bad Shodam',
+        path: 'assests\music\Yas - Bad Shodam - 320 - [RadicalMusic.Ir].mp3',
+        artist: 'ُYas(Ft Aamin)',
+        cover: 'assests\images\Yas-Boghz-Yani-Ft-Aamin.jpg'
+    },
+    {
+        name: '24 Saat',
+        path: 'assests\music\Bahram - 24 Saat.mp3',
+        artist: 'ُBahram',
+        cover: 'assests\images\24-saat-artwork.jpg'
+    },
+    {
+        name: 'Ehsasi',
+        path: 'assests\music\Purya Putak - Ehsasi [320].mp3',
+        artist: 'ُPurya Putak',
+        cover: 'assests\images\Purya-Putak-Ehsasi.jpg'
+    },
+    {
+        name: 'Adamak',
+        path: 'assests\music\Sadegh - Adamak.mp3',
+        artist: 'ُSadegh',
+        cover: 'assests\images\adamak-sadegh.jpg'
+    },
+]
